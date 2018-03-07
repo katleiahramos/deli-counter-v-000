4a9a614efe8e4283eb548eab551ca13e70e7dfbf
Katleiah <katleiahramos@gmail.com>
2018-03-07 19:02:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,6 @@
 # Write your code here.
 require 'pry'
 
-def take_a_number(array, name)
-  array.push(name)
-  place = array.length
-  puts "Welcome, #{name}. You are number #{place} in line."
-
-end
-
-
-=begin
 def line(katz_deli)
   if katz_deli.empty?
     return nil
@@ -17,4 +8,12 @@ def line(katz_deli)
 
     return "the line is currently:"
 end
-=end
+
+def take_a_number(array, name)
+  array.push(name)
+  place = array.length
+  puts "Welcome, #{name}. You are number #{place} in line."
+end
+
+
+
