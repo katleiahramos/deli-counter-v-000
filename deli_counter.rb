62713c6b399dbc01754068e62f510abbe22097ca
Katleiah <katleiahramos@gmail.com>
2018-03-07 18:57:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,11 +6,10 @@ def take_a_number(array, name)
   place = array.legnth
   puts "Welcome, #{name}. You are number #{place} in line."
   binding.pry
-
 end
 
 
-
+=begin 
 def line(katz_deli)
   if katz_deli.empty?
     return nil
@@ -18,3 +17,4 @@ def line(katz_deli)
 
     return "the line is currently:"
 end
+=end 
