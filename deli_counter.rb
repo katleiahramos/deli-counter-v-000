00aae2108d279836347d8121dba83e71436d9dd3
Katleiah <katleiahramos@gmail.com>
2018-03-07 18:55:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, Kent. You are number #{array.length} in line."
+  
+  
+
+
+end 
+
+
+
+def line(katz_deli) 
+  if katz_deli.empty? 
+    return nil 
+  else 
+    
+    return "the line is currently:"
+end 
+
